cffe1624be4390a1e614f398f285ca8ca9383ec4
user22813 <60642832+user22813@users.noreply.github.com>
2020-07-11 22:38:47 +0300

    Add files via upload

---
+++ get_proxy.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_proxy (url: str) -> str:
+	
+    a = requests.get(url)
+    filename = "proxies.txt"
+
+    file = open(filename, 'w+')
+    file.write(a.text) 
+    file.close()
+
+    return filename
